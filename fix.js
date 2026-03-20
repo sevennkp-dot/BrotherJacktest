b157218143cf7f0637189cfe4b7c688318d0a2e0
sevennkp <sevennkp@gmail.com>
2026-03-20 21:12:47 +0700

    Delete fix.js

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('technician.js', 'utf8');
-code = code.replace(/status: ".*"/g, 'status: "รอดำเนินการ"');
-fs.writeFileSync('technician.js', code);
-console.log('Fixed');
